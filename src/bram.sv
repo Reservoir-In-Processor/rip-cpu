59f04324a75a6c716e3ab7a4a6d9fda063047807
estodi <83288835+estodi@users.noreply.github.com>
2023-09-21 02:11:50 +0900

    [add] added bram implementation

---
+++ src/bram.sv
@@ -0,0 +1,45 @@
+`default_nettype none
+`timescale 1ns / 1ps
+
+// 
+// memory module implementation for FPGA Block RAM
+// 
+
+// Module: dual_read_bram
+// Description: 2-read 1-write bram
+module dual_read_bram #(
+  parameter DATA_WIDTH = 32, // bram data width
+  parameter ADDR_WIDTH = 10  // bram data depth
+) (
+  input wire clk,
+  input wire we,
+  input wire enable_1,
+  input wire enable_2,
+  input wire [ADDR_WIDTH-1:0] addr_1,
+  input wire [ADDR_WIDTH-1:0] addr_2,
+  input wire [DATA_WIDTH-1:0] din,
+  output reg [DATA_WIDTH-1:0] dout_1,
+  output reg [DATA_WIDTH-1:0] dout_2
+);
+
+  (* ram_style = "block" *)
+  logic [DATA_WIDTH-1:0] ram [(2 ** ADDR_WIDTH)-1:0];
+
+  always_ff @(posedge clk) begin
+    if (enable_1) begin
+      if (we) begin
+        ram[addr_1] <= din;
+      end
+      dout_1 <= ram[addr_1];
+    end
+  end
+
+  always_ff @(posedge clk) begin
+    if (enable_2) begin
+      dout_2 <= ram[addr_2];
+    end
+  end
+
+endmodule
+
+`default_nettype wire
