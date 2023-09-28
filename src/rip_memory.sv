23c5ef61d26fc11b683db38dbdd37ab74c67690d
sh-mug <52068539+sh-mug@users.noreply.github.com>
2023-09-28 19:20:22 +0900

    wip: set rip_memory interface

--- src/rip_memory.sv
+++ src/rip_memory.sv
@@ -4,25 +4,51 @@
 // Module: rip_memory
 // Description: byte addressing memory system top module.
 module rip_memory #(
-    parameter DATA_WIDTH = 32, // data port width
-    parameter ADDR_WIDTH = 32
+    parameter NUM_COL = 4,
+    parameter COL_WIDTH = 8,
+    parameter ADDR_WIDTH = 20,
+    parameter DATA_WIDTH = NUM_COL * COL_WIDTH  // data port width
 ) (
     input wire clk,
-    input wire rstn,
-    input inst instruction,
-    input wire we,
-    input wire re_1,
-    input wire re_2,
-    input wire [ADDR_WIDTH-1:0] addr_1,
-    input wire [DATA_WIDTH-1:0] din,
-    output wire [DATA_WIDTH-1:0] dout_1,
-    output wire [DATA_WIDTH-1:0] dout_2,
-    output wire busy
+    input wire rst_n,
+
+    input inst ma_inst,
+    input wire ma_en,
+    input wire [DATA_WIDTH-1:0] ma_addr,
+    input wire [DATA_WIDTH-1:0] ma_din,
+    output wire [DATA_WIDTH-1:0] ma_dout,
+
+    input wire if_en,
+    input wire [DATA_WIDTH-1:0] pc,
+    output wire [DATA_WIDTH-1:0] inst_code
 );
     import rip_const::*;
 
-    // TO BE IMPLEMENTED
+    integer i;
+    reg [DATA_WIDTH-1:0] mem_block[(1<<ADDR_WIDTH)-1:0];
+
+    initial begin
+        $readmemh("ram.hex", mem_block);
+    end
+
+    // instruction fetch
+    always @(posedge clkB) begin
+        if (if_en) begin
+            doutB <= ram_block[addrB];
+        end
+    end
+
+    // memory access
+    always @(posedge clk) begin
+        if (ma_en) begin
+            for (i = 0; i < NUM_COL; i = i + 1) begin
+                if (weA[i]) begin
+                    ram_block[addrA][i*COL_WIDTH+:COL_WIDTH] <= dinA[i*COL_WIDTH+:COL_WIDTH];
+                end
+            end
+            doutA <= ram_block[addrA];
+        end
+    end
+
 
 endmodule
-
-`default_nettype wire
