6bad9d9e5eabf37bb72a712a00a19712fa067e4c
estodi <83288835+estodi@users.noreply.github.com>
2023-11-20 21:30:16 +0900

    [add] added rip_branch_predictor

---
+++ src/rip_branch_predictor.sv
@@ -0,0 +1,25 @@
+`default_nettype none
+`timescale 1ns / 1ps
+
+//
+// branch predictor implementation
+//
+
+module rip_axi_master #(
+    // Pattern History Table
+    parameter PHT_LSB = 0,
+    parameter PHT_MSB = 31,
+    // Global History
+    parameter GLOBAL_HISTORY_DEPTH = 10
+) (
+    input wire [31:0] pc,
+    output logic pred,
+    input wire actual
+);
+
+    // to be implemented
+    assign pred = '0; // always taken
+
+endmodule
+
+`default_nettype wire
