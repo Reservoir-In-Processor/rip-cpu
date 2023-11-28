a7acf196fa73e8932f2effaeacb40c1e8c32533c
estodi <83288835+estodi@users.noreply.github.com>
2023-11-28 22:21:43 +0900

    [update] updated rip MMU io ports (also, renamed from rip_memory)

--- src/rip_memory_management_unit.sv
+++ src/rip_memory_management_unit.sv
@@ -1,27 +1,27 @@
 `default_nettype none
 `timescale 1ns / 1ps
 
-// Module: rip_memory_control_unit
+// Module: rip_memory_management_unit
 // Description: byte addressing memory system top module.
-module rip_memory_control_unit #(
+module rip_memory_management_unit
+    import rip_const::*;
+#(
     parameter DATA_WIDTH = 32, // data port width
     parameter ADDR_WIDTH = 32
 ) (
     input wire clk,
     input wire rstn,
-    input inst instruction,
-    input wire we_1,
+    input wire [DATA_WIDTH/B_WIDTH-1:0] we_1,
     input wire re_1,
     input wire re_2,
     input wire [ADDR_WIDTH-1:0] addr_1,
     input wire [ADDR_WIDTH-1:0] addr_2,
     input wire [DATA_WIDTH-1:0] din_1,
-    output wire [DATA_WIDTH-1:0] dout_1,
-    output wire [DATA_WIDTH-1:0] dout_2,
-    output wire busy_1,
-    output wire busy_2
+    output logic [DATA_WIDTH-1:0] dout_1,
+    output logic [DATA_WIDTH-1:0] dout_2,
+    output logic busy_1,
+    output logic busy_2
 );
-    import rip_const::*;
 
     // TO BE IMPLEMENTED
 
