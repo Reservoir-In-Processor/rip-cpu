4c739208499850fbefb28bf7f4a07ac5fd80c2f8
estodi <83288835+estodi@users.noreply.github.com>
2023-12-02 17:13:13 +0900

    [upgrade] upgraded 2r1w bram to support write strobes

--- src/rip_2r1w_bram.sv
+++ src/rip_2r1w_bram.sv
@@ -1,18 +1,20 @@
 `default_nettype none
 `timescale 1ns / 1ps
 
-// 
+//
 // memory module implementation for FPGA Block RAM primitive
-// 
+//
 
 // Module: rip_2r1w_bram
 // Description: 2-read 1-write bram
-module rip_2r1w_bram #(
+module rip_2r1w_bram
+    import rip_const::*;
+#(
   parameter DATA_WIDTH = 32, // bram data width
   parameter ADDR_WIDTH = 10  // bram data depth
 ) (
   input wire clk,
-  input wire we_1,
+  input wire [DATA_WIDTH/B_WIDTH-1:0] we_1,
   input wire enable_1,
   input wire enable_2,
   input wire [ADDR_WIDTH-1:0] addr_1,
@@ -27,9 +29,13 @@ module rip_2r1w_bram #(
 
   always_ff @(posedge clk) begin
     if (enable_1) begin
-      if (we_1) begin
-        ram[addr_1] <= din_1;
-      end
+      generate
+        for (genvar i = 0; i < DATA_WIDTH/B_WIDTH; i++) begin
+          if (we_1[i]) begin
+            ram[addr_1][i*B_WIDTH +: B_WIDTH] <= din_1[i*B_WIDTH +: B_WIDTH];
+          end
+        end
+      endgenerate
       dout_1 <= ram[addr_1];
     end
   end
